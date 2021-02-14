2f74b186c76a9b21a2b7b02d0ae39a82a0173f1a
Joshalosh <josh.jessopsmith@gmail.com>
2021-02-14 20:15:48 +1100

    Add gitignore and change build script flags

--- build.bat
+++ build.bat
@@ -4,6 +4,6 @@ IF NOT EXIST build mkdir build
 
 pushd build
 
-gcc ..\main.cpp -o generator.exe -O1 -Wall -std=c++11 -Wno-missing-braces -I ..\include/ -L ..\lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc ..\main.cpp -o generator.exe -g -Wall -std=c++11 -Wno-missing-braces -I ..\include/ -L ..\lib/ -lraylib -lopengl32 -lgdi32 -lwinmm
 
 popd
