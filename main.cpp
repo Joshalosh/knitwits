9d9395ef350eca4012af3b07760241b5d8044839
Joshalosh <josh.jessopsmith@gmail.com>
2021-02-18 22:38:11 +1100

    Check directory for filenames

--- main.cpp
+++ main.cpp
@@ -1,40 +1,44 @@
 #include "include/raylib.h"
 #include <stdio.h>
+#include <dirent.h>
 
 int main()
 {
-    int window_width  = 900;
-    int window_height = 600;
+    int window_width  = 1920;
+    int window_height = 1080;
 
     InitWindow(window_width, window_height, "MyGame");
     SetTargetFPS(60);
 
-    Texture2D scarfy = LoadTexture("../assets/scarfy.png");
 
-    float x = 32.0f, y = 32.0f;
+    Texture2D face = LoadTexture("../assets/face1.png");
+    Texture2D eyes = LoadTexture("../assets/eyes1.png");
+    Texture2D hat = LoadTexture("../assets/hat1.png");
+
+    //NOTE: Read all files in a directory
+    {
+        DIR *d;
+        struct dirent *dir;
+        d = opendir("../assets");
+
+        if(d)
+        {
+            while((dir = readdir(d)) != NULL)
+            {
+                printf("%s\n", dir->d_name);
+            }
+            closedir(d);
+        }
+    }
 
     while(!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-
-        if(IsGamepadAvailable(GAMEPAD_PLAYER1))
-        {
-            if(IsGamepadButtonPressed(GAMEPAD_PLAYER1, GAMEPAD_BUTTON_RIGHT_FACE_DOWN))
-            {
-                x += 30.0f;
-            }
-
-            float left_joystick_x = GetGamepadAxisMovement(GAMEPAD_PLAYER1, GAMEPAD_AXIS_LEFT_X);
-
-            x += GetFrameTime() * 100.0f * left_joystick_x;
-        }
-
-        DrawTexturePro(scarfy,
-                       Rectangle{0, 0, 128, 128},
-                       Rectangle{x, y, 128, 128},
-                       Vector2{0, 0}, 0, RAYWHITE);
+        DrawTexture(face, 0, 0, RAYWHITE);
+        DrawTexture(eyes, 0, 0, RAYWHITE);
+        DrawTexture(hat, 0, 0, RAYWHITE);
 
         EndDrawing();
     }
