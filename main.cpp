50255aae9f2a97ca2ff9b6e85eb4fd8b7465c88f
Joshalosh <josh.jessopsmith@gmail.com>
2021-03-02 22:08:29 +1100

    Count assets in seperate directories

--- main.cpp
+++ main.cpp
@@ -3,6 +3,33 @@
 #include "stdio.h"
 //#include "windows.h"
 
+#define internal static 
+#define global_variable static
+#define local_persist static
+
+internal int Directory_Count(char *filepath)
+{
+    DIR *d;
+    struct dirent *dir;
+    int file_count = 0;
+    d = opendir(filepath);
+    if(d)
+    {
+        while((dir = readdir(d)) != NULL)
+        {
+            file_count++;
+        }
+        closedir(d);
+        file_count -= 2;
+        return file_count;
+    }
+    else
+    {
+        return file_count;
+    }
+}
+
+
 int main()
 {
     int window_width  = 1920;
@@ -12,10 +39,17 @@ int main()
     SetTargetFPS(60);
 
 
-    Texture2D face = LoadTexture("../assets/face1.png");
-    Texture2D eyes = LoadTexture("../assets/eyes1.png");
-    Texture2D hat  = LoadTexture("../assets/hat1.png");
+    Texture2D face = LoadTexture("../assets/face/face1.png");
+    Texture2D eyes = LoadTexture("../assets/eyes/eyes1.png");
+    Texture2D hat  = LoadTexture("../assets/hat/hat1.png");
+    
+    char *face_filepath("../assets/face");
+    char *eyes_filepath("../assets/eyes");
+    char *hat_filepath("../assets/hat");
 
+    int max_faces = Directory_Count(face_filepath);
+    int max_eyes = Directory_Count(eyes_filepath);
+    int max_hats = Directory_Count(hat_filepath);
 
     //NOTE: Read all files in a directory
 #if 0
@@ -36,22 +70,6 @@ int main()
         }
     }
 #endif
-#if 1
-    {
-        DIR *d;
-        struct dirent *dir;
-        d = opendir("../assets");
-        if(d)
-        {
-            while((dir = readdir(d)) != NULL)
-            {
-                printf("%s\n", dir->d_name);
-            }
-            closedir(d);
-        }
-    }
-#endif
-
     while(!WindowShouldClose())
     {
         BeginDrawing();
